99f416ba4b3b350b79d5c3c2577d98f0b29ce4fc
pimpmaneaton <shaunsibu@gmail.com>
2019-07-08 10:50:11 -0600

    Build ims and qti telephony from source

--- common.mk
+++ common.mk
@@ -359,9 +359,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-# Telephony
 PRODUCT_PACKAGES += \
-    telephony-ext
+    ims-ext-common \
+    telephony-ext \
+    qti-telephony-common
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
