d242372fa54ad18ffa5107a5b0bd5d67ec1fe36c
pimpmaneaton <shaunsibu@gmail.com>
2019-07-13 20:15:56 -0600

    Hold off on oneplus camera for a moment

--- common.mk
+++ common.mk
@@ -378,16 +378,16 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Prebuilts
-PRODUCT_PACKAGES += \
-    OnePlusCamera \
-    OnePlusGallery \
-    OnePlusCameraService
+#PRODUCT_PACKAGES += \
+#    OnePlusCamera \
+#    OnePlusGallery \
+#    OnePlusCameraService
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/lib/libopcameralib-em.so:system/lib/libopcameralib-em.so  \
-    $(LOCAL_PATH)/prebuilts/lib/libstagefright.so:system/lib/libstagefright.so  \
-    $(LOCAL_PATH)/prebuilts/lib64/libstagefright.so:system/lib64/libstagefright.so  \
-    $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/prebuilts/lib/libopcameralib-em.so:system/lib/libopcameralib-em.so  \
+#    $(LOCAL_PATH)/prebuilts/lib/libstagefright.so:system/lib/libstagefright.so  \
+#    $(LOCAL_PATH)/prebuilts/lib64/libstagefright.so:system/lib64/libstagefright.so  \
+#    $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml
 
 # TextClassifier
 PRODUCT_PACKAGES += \
