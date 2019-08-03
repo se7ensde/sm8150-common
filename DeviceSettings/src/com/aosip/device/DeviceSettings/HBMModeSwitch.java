AOSiP-Devices/device_oneplus_sm8150-common

sm8150-common: DeviceSettings: Use utils from base
Change-Id: I9909efbf3187e0650b757305915e873a844c379c
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

DeviceSettings/src/com/aosip/device/DeviceSettings/HBMModeSwitch.java
@@ -23,31 +23,31 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceManager;
 
-import com.aosip.device.DeviceSettings.DeviceSettings;
+import com.android.internal.util.aosip.FileUtils;
 
 public class HBMModeSwitch implements OnPreferenceChangeListener {
 
     private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/hbm";
 
     public static String getFile() {
-        if (Utils.fileWritable(FILE)) {
+        if (FileUtils.fileWritable(FILE)) {
             return FILE;
         }
         return null;
     }
 
     public static boolean isSupported() {
-        return Utils.fileWritable(getFile());
+        return FileUtils.fileWritable(getFile());
     }
 
     public static boolean isCurrentlyEnabled(Context context) {
-        return Utils.getFileValueAsBoolean(getFile(), false);
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         Boolean enabled = (Boolean) newValue;
-        Utils.writeValue(getFile(), enabled ? "5" : "0");
+        FileUtils.writeValue(getFile(), enabled ? "5" : "0");
         return true;
     }
 }

DeviceSettings/src/com/aosip/device/DeviceSettings/HBMModeTileService.java
@@ -23,7 +23,7 @@ import android.content.SharedPreferences;
 import android.service.quicksettings.TileService;
 import android.support.v7.preference.PreferenceManager;
 
-import com.aosip.device.DeviceSettings.DeviceSettings;
+import com.android.internal.util.aosip.FileUtils;
 
 
 @TargetApi(24)
@@ -58,7 +58,7 @@ public class HBMModeTileService extends TileService {
         super.onClick();
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         final boolean enabled = HBMModeSwitch.isCurrentlyEnabled(this);
-        Utils.writeValue(HBMModeSwitch.getFile(), enabled ? "0" : "1");
+        FileUtils.writeValue(HBMModeSwitch.getFile(), enabled ? "0" : "1");
         sharedPrefs.edit().putBoolean(DeviceSettings.KEY_HBM_SWITCH, enabled ? false : true).commit();
     }
 }

DeviceSettings/src/com/aosip/device/DeviceSettings/PanelSettings.java
@@ -43,7 +43,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.util.Log;
 
-import com.aosip.device.DeviceSettings.DeviceSettings;
+import com.android.internal.util.aosip.FileUtils;
 
 public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCheckedChangeListener {
     private RadioGroup mRadioGroup;
@@ -81,49 +81,49 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
         SharedPreferences.Editor edit = sharedPrefs.edit();
         if (checkedId == R.id.srgb_mode) {
-            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            FileUtils.writeValue(DCIModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            FileUtils.writeValue(NightModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "0");
+            FileUtils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
-            Utils.writeValue(SRGBModeSwitch.getFile(), "1");
+            FileUtils.writeValue(SRGBModeSwitch.getFile(), "1");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, true);
         } else if (checkedId == R.id.dci_mode) {
-            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            FileUtils.writeValue(SRGBModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            FileUtils.writeValue(NightModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "0");
+            FileUtils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
-            Utils.writeValue(DCIModeSwitch.getFile(), "1");
+            FileUtils.writeValue(DCIModeSwitch.getFile(), "1");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, true);
         } else if (checkedId == R.id.night_mode) {
-            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            FileUtils.writeValue(SRGBModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            FileUtils.writeValue(DCIModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "0");
+            FileUtils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "1");
+            FileUtils.writeValue(NightModeSwitch.getFile(), "1");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, true);
         } else if (checkedId == R.id.off_mode) {
-            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            FileUtils.writeValue(DCIModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            FileUtils.writeValue(NightModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            FileUtils.writeValue(SRGBModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "0");
+            FileUtils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
         } else if (checkedId == R.id.wide_color_mode) {
-            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            FileUtils.writeValue(DCIModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            FileUtils.writeValue(NightModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            FileUtils.writeValue(SRGBModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "1");
+            FileUtils.writeValue(WideColorModeSwitch.getFile(), "1");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, true);
         }
         edit.commit();

DeviceSettings/src/com/aosip/device/DeviceSettings/Startup.java
@@ -25,7 +25,7 @@ import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
 import android.text.TextUtils;
 
-import com.aosip.device.DeviceSettings.DeviceSettings;
+import com.android.internal.util.aosip.FileUtils;
 
 public class Startup extends BroadcastReceiver {
 
@@ -34,7 +34,7 @@ public class Startup extends BroadcastReceiver {
             return;
         }
         if (enabled) {
-            Utils.writeValue(file, "1");
+            FileUtils.writeValue(file, "1");
         }
     }
 
@@ -42,7 +42,7 @@ public class Startup extends BroadcastReceiver {
         if (file == null) {
             return;
         }
-        Utils.writeValue(file, value);
+        FileUtils.writeValue(file, value);
     }
 
     @Override

se7ensde/sm8150-common

DeviceSettings/src/com/aosip/device/DeviceSettings/HBMModeSwitch.java
@@ -23,31 +23,31 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceManager;
 
-import com.aosip.device.DeviceSettings.DeviceSettings;
+import com.android.internal.util.bliss.FileUtils;
 
 public class HBMModeSwitch implements OnPreferenceChangeListener {
 
     private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/hbm";
 
     public static String getFile() {
-        if (Utils.fileWritable(FILE)) {
+        if (FileUtils.fileWritable(FILE)) {
             return FILE;
         }
         return null;
     }
 
     public static boolean isSupported() {
-        return Utils.fileWritable(getFile());
+        return FileUtils.fileWritable(getFile());
     }
 
     public static boolean isCurrentlyEnabled(Context context) {
-        return Utils.getFileValueAsBoolean(getFile(), false);
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         Boolean enabled = (Boolean) newValue;
-        Utils.writeValue(getFile(), enabled ? "5" : "0");
+        FileUtils.writeValue(getFile(), enabled ? "5" : "0");
         return true;
     }
 }

DeviceSettings/src/com/aosip/device/DeviceSettings/HBMModeTileService.java
@@ -23,7 +23,7 @@ import android.content.SharedPreferences;
 import android.service.quicksettings.TileService;
 import android.support.v7.preference.PreferenceManager;
 
-import com.aosip.device.DeviceSettings.DeviceSettings;
+import com.android.internal.util.bliss.FileUtils;
 
 
 @TargetApi(24)
@@ -58,7 +58,7 @@ public class HBMModeTileService extends TileService {
         super.onClick();
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         final boolean enabled = HBMModeSwitch.isCurrentlyEnabled(this);
-        Utils.writeValue(HBMModeSwitch.getFile(), enabled ? "0" : "1");
+        FileUtils.writeValue(HBMModeSwitch.getFile(), enabled ? "0" : "1");
         sharedPrefs.edit().putBoolean(DeviceSettings.KEY_HBM_SWITCH, enabled ? false : true).commit();
     }
 }

DeviceSettings/src/com/aosip/device/DeviceSettings/PanelSettings.java
@@ -43,7 +43,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.util.Log;
 
-import com.aosip.device.DeviceSettings.DeviceSettings;
+import com.android.internal.util.bliss.FileUtils;
 
 public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCheckedChangeListener {
     private RadioGroup mRadioGroup;
@@ -81,49 +81,49 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
         SharedPreferences.Editor edit = sharedPrefs.edit();
         if (checkedId == R.id.srgb_mode) {
-            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            FileUtils.writeValue(DCIModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            FileUtils.writeValue(NightModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "0");
+            FileUtils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
-            Utils.writeValue(SRGBModeSwitch.getFile(), "1");
+            FileUtils.writeValue(SRGBModeSwitch.getFile(), "1");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, true);
         } else if (checkedId == R.id.dci_mode) {
-            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            FileUtils.writeValue(SRGBModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            FileUtils.writeValue(NightModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "0");
+            FileUtils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
-            Utils.writeValue(DCIModeSwitch.getFile(), "1");
+            FileUtils.writeValue(DCIModeSwitch.getFile(), "1");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, true);
         } else if (checkedId == R.id.night_mode) {
-            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            FileUtils.writeValue(SRGBModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            FileUtils.writeValue(DCIModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "0");
+            FileUtils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "1");
+            FileUtils.writeValue(NightModeSwitch.getFile(), "1");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, true);
         } else if (checkedId == R.id.off_mode) {
-            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            FileUtils.writeValue(DCIModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            FileUtils.writeValue(NightModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            FileUtils.writeValue(SRGBModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "0");
+            FileUtils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
         } else if (checkedId == R.id.wide_color_mode) {
-            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            FileUtils.writeValue(DCIModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            FileUtils.writeValue(NightModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            FileUtils.writeValue(SRGBModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "1");
+            FileUtils.writeValue(WideColorModeSwitch.getFile(), "1");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, true);
         }
         edit.commit();

DeviceSettings/src/com/aosip/device/DeviceSettings/Startup.java
@@ -25,7 +25,7 @@ import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
 import android.text.TextUtils;
 
-import com.aosip.device.DeviceSettings.DeviceSettings;
+import com.android.internal.util.bliss.FileUtils;
 
 public class Startup extends BroadcastReceiver {
 
@@ -34,7 +34,7 @@ public class Startup extends BroadcastReceiver {
             return;
         }
         if (enabled) {
-            Utils.writeValue(file, "1");
+            FileUtils.writeValue(file, "1");
         }
     }
 
@@ -42,7 +42,7 @@ public class Startup extends BroadcastReceiver {
         if (file == null) {
             return;
         }
-        Utils.writeValue(file, value);
+        FileUtils.writeValue(file, value);
     }
 
     @Override