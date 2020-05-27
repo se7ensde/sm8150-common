e81cb2bd6db74b609f93419e40bb0554d3e7955f
Ashwin R C <ashwin2001achu@gmail.com>
2020-05-27 15:17:10 +0000

    sm8150-common: KeyHandler: Use VibrationEffect
    * Also update haptic feedback logic

--- DeviceSettings/src/com/aosip/device/DeviceSettings/KeyHandler.java
+++ DeviceSettings/src/com/aosip/device/DeviceSettings/KeyHandler.java
@@ -40,6 +40,7 @@ import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.Vibrator;
+import android.os.VibrationEffect;
 import android.provider.Settings;
 import android.util.Log;
 import android.util.SparseIntArray;
@@ -141,7 +142,7 @@ public class KeyHandler implements DeviceKeyHandler {
     public KeyEvent handleKeyEvent(KeyEvent event) {
         int scanCode = event.getScanCode();
         String keyCode = Constants.sKeyMap.get(scanCode);
-        
+
         int keyCodeValue = 0;
         try {
             keyCodeValue = Constants.getPreferenceInt(mContext, keyCode);
@@ -175,10 +176,10 @@ public class KeyHandler implements DeviceKeyHandler {
     }
 
     private void doHapticFeedback() {
-        if (mVibrator == null) {
-            return;
+        if (mVibrator != null && mVibrator.hasVibrator()) {
+            mVibrator.vibrate(VibrationEffect.createOneShot(50,
+                    VibrationEffect.DEFAULT_AMPLITUDE));
         }
-	mVibrator.vibrate(50);
     }
 
     public void handleNavbarToggle(boolean enabled) {
